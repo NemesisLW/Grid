425446edff0ccc2081213aff476808202451b32d
Sudiptadas638 <saicelldevelopers@gmail.com>
2023-08-12 22:17:30 +0530

    second push

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -7,7 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
-
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import Image from "next/image";
 const Avatar = () => {
   return (
@@ -24,7 +32,21 @@ const Avatar = () => {
                 width={50}
                 height={50}
               />{" "}
-             
+               <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
             </CardContent>
           </CardHeader>
         </Card>
